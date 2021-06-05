3aeb6ffe390b7790ee85d0bc00e9597b1cd95089
Vitali-Leikin <leikin1.19@gmail.com>
2021-06-05 20:33:52 +0300

    Task 3 test 1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,18 +1,26 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var arrayUser: [User] = []
+       var myCalls: [Call] = []
+}
 
 extension CallStation: Station {
+    
     func users() -> [User] {
-        []
+        return arrayUser
+        
     }
     
     func add(user: User) {
-
+        if !self.arrayUser.contains(user) {
+                    self.arrayUser.append(user)
+                }
     }
     
     func remove(user: User) {
-
+        
+       arrayUser.removeAll()
     }
     
     func execute(action: CallAction) -> CallID? {
